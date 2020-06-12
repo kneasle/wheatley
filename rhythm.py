7a87f3b8f75857cb0acfb70f3807f661a63819ae
Kneasle <kneasle@gmail.com>
2020-06-12 20:00:59 +0100

    Added data collection to Rhythm so that will be able to adjust to other people

--- rhythm.py
+++ rhythm.py
@@ -9,11 +9,15 @@ class Rhythm:
         self._blow_interval = 0
 
         self._expected_bells = {}
+        self.data_set = []
 
-    def expect_bell (self, expected_bell, expected_time, expected_stroke):
-        print (f"Expecting {expected_bell} at {expected_time} on {'H' if expected_stroke else 'B'}")
+    def expect_bell (self, expected_bell, expected_blow_time, expected_stroke):
+        self._expected_bells [(expected_bell, expected_stroke)] = expected_blow_time
 
-        self._expected_bells [expected_bell] = (expected_time, expected_stroke)
+    def add_data_point (self, blow_time, real_time):
+        self.data_set.append ((blow_time, real_time))
+        
+        print (f"Dataset: {self.data_set}")
 
     def on_bell_ring (self, bell, stroke, real_time):
         if (bell, stroke) in self._expected_bells:
@@ -22,6 +26,13 @@ class Rhythm:
             diff = self.real_time_to_blow_time (real_time) - expected_blow_time
 
             print (f"Off by {diff} places")
+
+            # Add the data point to our list if either it is close enough to not be an anomaly
+            # or there are fewer than 2 datapoints in our dataset, in which case we cannot
+            # determine the line accurately so we should add the datapoint anyway on the 
+            # basis that it's better than nothing
+            if len (self.data_set) <= 1 or abs (diff) <= 0.5:
+                self.add_data_point (expected_blow_time, real_time)
 
             del self._expected_bells [(bell, stroke)]
         else:
@@ -36,6 +47,9 @@ class Rhythm:
             10: 0.3,
             12: 0.2
         } [self._bot.stage]
+
+        self.data_set = []
+        self.add_data_point (0, start_real_time)
 
 
     # Linear algebra-style conversions between different time measurements

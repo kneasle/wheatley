274ff6ea470d9ffd8382a912e17d7dd752c0b1eb
Kneasle <kneasle@gmail.com>
2020-08-12 15:57:42 +0100

    Add a fuzzer for peal speed

---
+++ fuzzing/peal_speed_parsing.py
@@ -0,0 +1,19 @@
+""" Run a fuzzer to generate lots of random input for the peal speed parser. """
+
+from random import choice, randint
+
+from wheatley.arg_parsing import parse_peal_speed, PealSpeedParseError
+
+from .fuzz_utils import fuzz_for_unwrapped_errors
+
+def random_peal_speed_string():
+    """ Generate a plausable input value for `wheatley.arg_parsing.parse_peal_speed` to parse. """
+
+    return "".join([choice("1234567890hm,\nx?ET ") for _ in range(randint(0, 20))])
+
+fuzz_for_unwrapped_errors(
+    "parse_peal_speed",
+    parse_peal_speed,
+    random_peal_speed_string,
+    PealSpeedParseError
+)

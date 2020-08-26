eae68691db8f5a6dced008252f424e8516fde322
Kneasle <kneasle@gmail.com>
2020-08-26 17:30:06 +0100

    Remove the old server_port/server_mode code

--- wheatley/main.py
+++ wheatley/main.py
@@ -279,8 +279,7 @@ def main(override_args=None, stop_on_join_tower=False):
     configure_logging()
 
     try:
-        url = "http://127.0.0.1:" + str(args.server_port) if args.server_mode else args.url
-        tower_url = get_load_balancing_url(args.room_id, url)
+        tower_url = get_load_balancing_url(args.room_id, args.url)
     except TowerNotFoundError as e:
         sys.exit(f"Bad value for 'room_id': {e}")
     except InvalidURLError as e:

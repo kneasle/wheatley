1c504cdd607ef7ba1b47073d66a3c3174e5bf380
Kneasle <kneasle@gmail.com>
2020-08-31 19:44:55 +0100

    Have Rhythm classes respond to 'sensitivity' change

--- wheatley/rhythm.py
+++ wheatley/rhythm.py
@@ -137,6 +137,9 @@ class WaitForUserRhythm(Rhythm):
     def change_setting(self, key, value):
         self._inner_rhythm.change_setting(key, value)
 
+        if key == "sensitivity":
+            self.logger.warning(f"NOT IMPLEMENTED: setting sensitivity to {value}.")
+
     def on_bell_ring(self, bell, stroke, real_time):
         """
         Called when a bell is rung at a given stroke.  Used as a callback from the Tower class.
@@ -279,6 +282,9 @@ class RegressionRhythm(Rhythm):
     def change_setting(self, key, value):
         def log_warning(message):
             self.logger.warning(f"Invalid value for setting '{key}': {message}")
+
+        if key == "sensitivity":
+            self.logger.warning(f"NOT IMPLEMENTED: setting sensitivity to {value}.")
 
         if key == 'inertia':
             try:

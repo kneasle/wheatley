3d90c135c3b664acca0c9692b9854638209b3266
Kneasle <kneasle@gmail.com>
2020-10-22 22:28:53 +0100

    Move Rhythm classes to their own files

---
+++ wheatley/rhythm/__init__.py
@@ -0,0 +1,5 @@
+""" A module to hold various classes determining when Wheatley rings his bells. """
+
+from .wait_for_user import WaitForUserRhythm
+from .regression import RegressionRhythm
+from .abstract_rhythm import Rhythm

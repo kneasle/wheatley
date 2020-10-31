6410c845f77b54d9f642131720f7b38665e17798
Kneasle <kneasle@gmail.com>
2020-10-31 13:07:08 +0000

    Update unit tests to the new API

--- tests/row_generation/generator_test_helpers.py
+++ tests/row_generation/generator_test_helpers.py
@@ -6,19 +6,19 @@ from wheatley.stroke import Stroke, HANDSTROKE
 
 def rounds(stage: int) -> List[int]:
     number_of_bells = stage + 1 if stage % 2 else stage
-    return [i for i in range(1, number_of_bells + 1)]
+    return list(range(1, number_of_bells + 1))
 
 
 def as_bells(nums: List[int]) -> List[Bell]:
     return [Bell.from_number(x) for x in nums]
 
 
-def gen_rows(generator, count, start_stroke: Stroke = HANDSTROKE) -> List[int]:
-    return list(yield_rows(generator, count, start_stroke))
+def gen_rows(generator, count) -> List[int]:
+    return list(yield_rows(generator, count))
 
 
-def yield_rows(generator, count, start_stroke=HANDSTROKE) -> Iterator[int]:
-    stroke = start_stroke
+def yield_rows(generator, count) -> Iterator[int]:
+    stroke = generator.start_stroke()
     for _ in range(count):
-        yield [bell.number for bell in generator.next_row(stroke)]
+        yield [bell.number for bell in generator.next_row_and_calls(stroke)[0]]
         stroke = stroke.opposite()
